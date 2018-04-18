82175c2dbf275b17cbcf53ea4e49abdc4d24941d
Evgeny Krukov <ekryukov@lanit.ru>
2018-04-18 12:22:52 +0300

    Fix unittest for acm, add fixtures for auth and acm models

--- sv_core/core/acm/tests.py
+++ sv_core/core/acm/tests.py
@@ -11,11 +11,15 @@ class AccessManagementTestCase(TestCase):
             'username': 'testuser',
             'password': 'secret'}
         user = User.objects.create_user(**self.credentials)
-        empl = Profile(user=user, lang='LANGRUS')
+        empl = Profile.objects.get(user=user)
+        empl.location = "Moscow"
+        empl.lang = 'LANGRUS'
+        empl.skype = 'skype_acc'
         empl.save()
 
     def test_login_page(self):
         response = self.client.post(reverse('login'), self.credentials, follow=True)
         # should be logged in now
         self.assertTrue(response.context['user'].is_active)
+        self.assertEqual(response.context['user'].profile.skype, 'skype_acc')
 
